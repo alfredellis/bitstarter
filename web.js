34fb869c80ad0793edc5a60ae390f42e35cb592d
Alfred Ellis <alfredellis@gmail.com>
2013-07-06 18:02:25 +0000

    Trying to trigger heroku deployment

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
 
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var MyHello = new Buffer(256);
+  MyHello = fs.readFileSync( 'index.html' );
+  response.send(MyHello.toString());
 });
 
 var port = process.env.PORT || 5000;
